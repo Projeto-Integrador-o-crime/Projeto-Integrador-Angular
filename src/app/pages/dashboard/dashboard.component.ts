372f3131d3361678ab9bdb57ea9613670d3718c6
FreyRin <guilhermebertoldo58@gmail.com>
2024-03-21 20:29:44 -0300

    add Dashboard-nav&header V2.2.1

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatExpansionModule,RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  panelOpenState = false;
 }
