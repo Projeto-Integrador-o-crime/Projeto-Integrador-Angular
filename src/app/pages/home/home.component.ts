c8833bdb6625b8d69dc7f1bbbe98358f77a0a533
ysabrinadev <ysabrina.dev@gmail.com>
2024-06-19 20:32:27 -0300

    [Feature] Introdução ao desenvolvimento do sistema de cookies. V1.1.8.9

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import { NavComponent } from '../../components/nav/nav.component';
 import { ServicosComponent } from '../../components/servicos/servicos.component';
 import { SobreComponent } from '../../components/sobre/sobre.component';
 import { CarouselTopComponent } from '../../components/carousel-top/carousel-top.component';
+import { CookiesComponent } from '../../components/cookies/cookies.component';
 
 @Component({
   selector: 'app-home',
@@ -14,8 +15,11 @@ import { CarouselTopComponent } from '../../components/carousel-top/carousel-top
     CarouselTopComponent,
     ServicosComponent,
     SobreComponent,
+    CookiesComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
+
 export class HomeComponent {}
+
