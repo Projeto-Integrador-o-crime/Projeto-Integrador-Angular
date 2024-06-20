05c5c77f047f42ba87bf3b276bc11a5b9fb45d67
siay-y <luiz.porfiriosantos@gmail.com>
2024-06-19 21:31:34 -0300

    [feature] add join us and footer to home page V.1.3.8.9

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,6 +4,8 @@ import { NavComponent } from '../../components/nav/nav.component';
 import { ServicosComponent } from '../../components/servicos/servicos.component';
 import { SobreComponent } from '../../components/sobre/sobre.component';
 import { CarouselTopComponent } from '../../components/carousel-top/carousel-top.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { JoinUsComponent } from '../../components/join-us/join-us.component';
 
 @Component({
   selector: 'app-home',
@@ -14,8 +16,10 @@ import { CarouselTopComponent } from '../../components/carousel-top/carousel-top
     CarouselTopComponent,
     ServicosComponent,
     SobreComponent,
+    FooterComponent,
+    JoinUsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent { }
